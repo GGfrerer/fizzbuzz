12440c0d30d878b56a7cd4df15194c5fcec857cf
GGfrerer <ggfrerer@gmail.com>
2016-09-13 18:07:21 +0200

    first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,23 @@
+zahl = 101
+
+while zahl > 100:
+    zahl = int(raw_input("Select a number between 1 and 100: "))
+    print "The number must be between 1 and 100!"
+
+    if zahl <= 100:
+        print
+        print "Your number is " + str(zahl)
+
+        for x in range(zahl):
+            y=x+1
+
+            if (y%3) + (y%5) == 0:
+                print "fizzbuzz"
+            elif y%3 == 0:
+                print "fizz"
+            elif y%5 == 0:
+                print "buzz"
+            else:
+                print y
+        break
+
