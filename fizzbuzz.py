c15a973959d6acc926bcabcc516c34cd5907c4ef
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2016-09-15 18:15:48 +0200

    Fix checking for zahl < 1 or zahl > 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
 zahl = 101
 
-while zahl > 100:
+while True:
     zahl = int(raw_input("Select a number between 1 and 100: "))
-    print "The number must be between 1 and 100!"
 
-    if zahl <= 100:
+    if zahl < 1 or zahl > 100:
+        print "The number must be between 1 and 100!"
+    else:
         print
         print "Your number is " + str(zahl)
         print
@@ -21,5 +22,3 @@ while zahl > 100:
             else:
                 print y
         break
-
-
